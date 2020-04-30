b6a25bb7d84e6cce000c39d6a81c222662d1d1d3
fangzhhc <fangzhhc@yonyou.com>
2020-04-30 15:31:48 +0800

    测试husky eslint

--- src/component/upload.jsx
+++ src/component/upload.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { DatePicker } from 'antd';
 
 class Upload extends React.Component {
+    handleClick =()=>{
+        console.log("1121")
+    }
     render(){
         return <div>
+            <div onClick={this.handleClick}>qewq</div>
             <DatePicker/>
         </div>
     }
