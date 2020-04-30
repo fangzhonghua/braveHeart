bbce047d704cfb554e9f83f009073bc290c116fe
fangzhhc <fangzhhc@yonyou.com>
2020-04-30 15:46:13 +0800

    测试husky eslint格式规则检验

--- src/route.jsx
+++ src/route.jsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
-import Home from './component/home.jsx';
-import Start from './component/start.jsx';
-import Upload from './component/upload.jsx';
-import NoMatch from './component/noMatch.jsx';
+import React from "react";
+import { HashRouter, Route, Switch } from "react-router-dom";
+import Home from "./component/home.jsx";
+import Start from "./component/start.jsx";
+import Upload from "./component/upload.jsx";
+import NoMatch from "./component/noMatch.jsx";
 
 const Routes = () => {
-    return <HashRouter>
-        <Switch>
-            <Route exact path="/">
-                <Home />
-            </Route>
-            <Route path="/start">
-                <Start />
-            </Route>
-            <Route path="/upload">
-                <Upload />
-            </Route>
-            <Route path="*" >
-                <NoMatch />
-            </Route>
-        </Switch>
-    </HashRouter >
-}
+  return <HashRouter>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/start">
+        <Start />
+      </Route>
+      <Route path="/upload">
+        <Upload />
+      </Route>
+      <Route path="*" >
+        <NoMatch/>
+      </Route>
+    </Switch>
+  </HashRouter>;
+};
 
 export default Routes;
