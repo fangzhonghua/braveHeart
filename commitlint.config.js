3c73f4b7e571ee097b6006bb1a75a5817dae7cd0
方中华 <f0322zh@163.com>
2020-05-08 17:34:16 +0800

    docs: 提交类型列举注释

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,18 +1,31 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'] // 官方推荐的扩展规则，常用都有够用
 };
-// 构建  build,
-// 杂务  chore,
-// ci   ci,
-// 文档  docs,
-// 专长  feat,
-// 修复  fix,
-// 改进  improvement,
-// 性能  perf,
-// 重构  refactor,
-// 还原  revert,
-// 风格  style,
-// 测试  test
+//构建 build,
+//杂务 chore, 
+//ci   ci,
+//文档 docs,
+//功能 feat,
+//修复 fix,
+//改进 improvement,
+//性能 perf,
+//重构 refactor,
+//还原 revert,
+//样式 style,
+//测试 test
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
