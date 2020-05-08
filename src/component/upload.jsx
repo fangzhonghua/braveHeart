adcda0611ceddfd4e038498a1d65edb8068e6207
方中华 <f0322zh@163.com>
2020-05-08 16:10:10 +0800

    style

--- src/component/upload.jsx
+++ src/component/upload.jsx
@@ -1,33 +1,31 @@
 import React from "react";
-import { createBrowserHistory } from "history";
-import { Upload, message, Button,DatePicker } from 'antd';
-const history = createBrowserHistory();
+import { Upload, message, Button, DatePicker } from "antd";
 class UploadPage extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {};
   }
   handleClick = () => {
     location.href = "/start";
   }
   handleChange = (info) => {
-    if (info.file.status !== 'uploading') {
+    if (info.file.status !== "uploading") {
       console.log(info.file, info.fileList);
     }
-    console.log("info",info)
-    if (info.file.status === 'done') {
+    console.log("info", info);
+    if (info.file.status === "done") {
       // message.success(`${info.file.name} file uploaded successfully`);
-      
-    } else if (info.file.status === 'error') {
+
+    } else if (info.file.status === "error") {
       message.error(`${info.file.name} file upload failed.`);
     }
   }
   render() {
     const props = {
-      name: 'file',
-      action: 'http://localhost:8888/api/upload',
+      name: "file",
+      action: "http://localhost:8888/api/upload",
       headers: {
-        authorization: 'authorization-text',
+        authorization: "authorization-text",
       },
       multiple: true
     };
